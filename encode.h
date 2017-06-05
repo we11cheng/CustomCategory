811ac527a0a1eb96f915276cec7d33ea980e7998
guanweicheng <1396539283@qq.com>
2017-06-05 20:59:43 +0800

    快速归档解归档

---
+++ encode.h
@@ -0,0 +1,45 @@
+//
+//  encode.h
+//  MineHeadDemo
+//
+//  Created by gwc on 05/06/2017.
+//  Copyright © 2017 jixiang. All rights reserved.
+//
+
+#ifndef encode_h
+#define encode_h
+
+#import <objc/runtime.h>
+
+#define encodeRuntime(A) \
+\
+unsigned int count = 0;\
+Ivar *ivars = class_copyIvarList([A class], &count);\
+for (int i = 0; i<count; i++) {\
+Ivar ivar = ivars[i];\
+const char *name = ivar_getName(ivar);\
+NSString *key = [NSString stringWithUTF8String:name];\
+id value = [self valueForKey:key];\
+[encoder encodeObject:value forKey:key];\
+}\
+free(ivars);\
+\
+
+#define initCoderRuntime(A) \
+\
+if (self = [super init]) {\
+unsigned int count = 0;\
+Ivar *ivars = class_copyIvarList([A class], &count);\
+for (int i = 0; i<count; i++) {\
+Ivar ivar = ivars[i];\
+const char *name = ivar_getName(ivar);\
+NSString *key = [NSString stringWithUTF8String:name];\
+id value = [decoder decodeObjectForKey:key];\
+[self setValue:value forKey:key];\
+}\
+free(ivars);\
+}\
+return self;\
+\
+
+#endif /* encode_h */
