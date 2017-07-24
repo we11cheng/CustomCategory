988bd00b0833aaa8ba3f6cf66c402c3d9714776d
guanweicheng <1396539283@qq.com>
2017-07-24 15:59:34 +0800

    任意视图角标

---
+++ 任意视图角标/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yeebadge
+//
+//  Created by Yee on 2017/7/9.
+//  Copyright © 2017年 CoderYee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
