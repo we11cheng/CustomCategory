1a69c120c414ed40f8ceba8837642fa4f4c6cbc6
guanweicheng <1396539283@qq.com>
2017-11-24 16:24:24 +0800

    encode decode

---
+++ Urlencodecode/NSString+Gwc_urlcode.h
@@ -0,0 +1,23 @@
+//
+//  NSString+Gwc_urlcode.h
+//  TestGwc
+//
+//  Created by admin on 24/11/2017.
+//  Copyright © 2017 guanweicheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Gwc_urlcode)
+
+/**
+ *  URLEncode
+ */
+- (NSString *)URLEncodedString;
+
+/**
+ *  URLDecode
+ */
+-(NSString *)URLDecodedString; 
+
+@end
---
+++ Urlencodecode/NSString+Gwc_urlcode.m
@@ -0,0 +1,47 @@
+//
+//  NSString+Gwc_urlcode.m
+//  TestGwc
+//
+//  Created by admin on 24/11/2017.
+//  Copyright © 2017 guanweicheng. All rights reserved.
+//
+
+#import "NSString+Gwc_urlcode.h"
+
+@implementation NSString (Gwc_urlcode)
+
+/**
+ *  URLEncode
+ */
+- (NSString *)URLEncodedString
+{
+    // CharactersToBeEscaped = @":/?&=;+!@#$()~',*";
+    // CharactersToLeaveUnescaped = @"[].";
+    
+    NSString *unencodedString = self;
+    NSString *encodedString = (NSString *)
+    CFBridgingRelease(CFURLCreateStringByAddingPercentEscapes(kCFAllocatorDefault,
+                                                              (CFStringRef)unencodedString,
+                                                              NULL,
+                                                              (CFStringRef)@"!*'();:@&=+$,/?%#[]",
+                                                              kCFStringEncodingUTF8));
+    
+    return encodedString;
+}
+
+/**
+ *  URLDecode
+ */
+-(NSString *)URLDecodedString
+{
+    //NSString *decodedString = [encodedString stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding ];
+    
+    NSString *encodedString = self;
+    NSString *decodedString  = (__bridge_transfer NSString *)CFURLCreateStringByReplacingPercentEscapesUsingEncoding(NULL,
+                                                                                                                     (__bridge CFStringRef)encodedString,
+                                                                                                                     CFSTR(""),
+                                                                                                                     CFStringConvertNSStringEncodingToEncoding(NSUTF8StringEncoding));
+    return decodedString;
+}  
+
+@end
